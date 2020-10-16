efe9802d5a8d59bbd846dc519d820719d7a2fde6
Aaron Daughtry <aarondaughtry@gmail.com>
2020-10-16 19:00:01 -0400

    Added countdown function and shortened delay and wait times.

--- plug_random.py
+++ plug_random.py
@@ -1,16 +1,26 @@
 import asyncio
 from kasa import SmartPlug
 from random import randint
-import time
+from time import sleep
 
 # Initial delay in seconds.
-DELAY = 10 * 60
+DELAY = 5 * 60
 # Run time min and max in seconds.
 RUN_MIN = 3
 RUN_MAX = 30
 # Wait time min and max in seconds.
 WAIT_MIN = 30
-WAIT_MAX = 5 * 60
+WAIT_MAX = 3 * 60
+
+def countdown(t):
+    mins = t // 60
+    secs = t - (mins * 60)
+    for m in range(mins, -1, -1):
+        for s in range(secs, -1, -1):
+            print("  {:02d}:{:02d}".format(m, s), end="\r")
+            sleep(1)
+        secs = 59
+
 
 async def main():
     plug_ip = input("Enter plug IP address: ")
@@ -22,14 +32,15 @@ async def main():
         exit()
     
     await plug.turn_off()
-    time.sleep(DELAY)
+    print("Beginning countdown.")
+    countdown(DELAY)
     while True:
         run_time = randint(RUN_MIN, RUN_MAX)
         wait_time = randint(WAIT_MIN, WAIT_MAX)
         await plug.turn_on()
-        time.sleep(run_time)
+        sleep(run_time)
         await plug.turn_off()
-        time.sleep(wait_time)
+        sleep(wait_time)
 
 if __name__ == '__main__':
     asyncio.run(main())
